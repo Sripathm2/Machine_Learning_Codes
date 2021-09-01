e1c6db7c2135a77e5ec3dabc97ab443c2258177b
Sripath Mishra <Sripathm2@gmail.com>
2021-09-01 11:30:32 -0700

    removing test.py as it is no longer required.

--- test.py
+++
@@ -1,6 +0,0 @@
-from torchvision import datasets, transforms
-
-transform = transforms.ToTensor()
-
-train_data = datasets.MNIST(root='../Data', train=True, download=True, transform=transform)
-test_data = datasets.MNIST(root='../Data', train=False, download=True, transform=transform)
